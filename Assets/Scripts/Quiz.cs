c161e6604199b23177acdb37d7d618cd6b4e73c3
Joshua Cory <accounts@keeplooking.net>
2024-03-07 17:36:50 -0700

    made button reset and disable/re-enable methods

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class Quiz : MonoBehaviour
 {
@@ -12,8 +13,59 @@ public class Quiz : MonoBehaviour
     int correctAnswerIndex;
     
    [SerializeField] Sprite defaultAnswerSprite;
-    [SerializeField] Sprite correctAnswerSprite;
+   [SerializeField] Sprite correctAnswerSprite;
     void Start()
+    {
+        GetNextQuestion();
+       //DisplayQuestion(); 
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        //Image buttonImage;
+        
+        if(index == question.GetcorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+           SetDefaltButtonSprite(correctAnswerSprite, index, false);
+        }
+        else
+        {
+            correctAnswerIndex = question.GetcorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Good try, but the correct answer is :\n" + correctAnswer;
+            SetDefaltButtonSprite(correctAnswerSprite, correctAnswerIndex, false);
+
+        }
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()  //untested
+    {
+        SetButtonState(true);
+        SetDefaltButtonSprite(defaultAnswerSprite, 0, true);
+        DisplayQuestion();
+    }
+
+    private void SetDefaltButtonSprite(Sprite sprite, int index, bool cycle)
+    {
+        Image buttonImage;
+        if(cycle == true)
+        {
+            for(int i = 0; i < answerButtons.Length; i++ )
+            {
+                buttonImage = answerButtons[i].GetComponent<Image>();
+                buttonImage.sprite = sprite;
+            }
+        }
+        else
+        {
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = sprite;
+        }
+    }
+
+    void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -24,14 +76,13 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    public void OnAnswerSelected(int index)
+    void SetButtonState(bool state)
     {
-        Image buttonImage;
-        if(index == question.GetcorrectAnswerIndex())
+        for(int i = 0; i < answerButtons.Length; i++)
         {
-            questionText.text = "Correct";
-            buttonImage = answerButtons[index].GetComponent<Image>();
-            buttonImage.sprite = correctAnswerSprite;
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
         }
     }
+
 }
