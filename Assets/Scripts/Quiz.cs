5f68eb894d51867fdd034da03b569569a1188aeb
Joshua Cory <accounts@keeplooking.net>
2024-08-15 11:26:05 -0600

    update and fix on scrolling rect

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -83,7 +83,7 @@ public class Quiz : MonoBehaviour
         {
             correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
             string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
-            questionText.text = "Good try, but the correct answer is :\n" + correctAnswer + " :\n" + currentQuestion.GetDiscussionAnswer();
+            questionText.text = "Good try, but the correct answer is: " + "[" + correctAnswer + "]\n" + currentQuestion.GetDiscussionAnswer();
             SetDefaultButtonSprite(correctAnswerSprite, correctAnswerIndex, false);
             if(index >= 0 && index <= answerButtons.Length)
             {
